dd17c7a60c290b6c7d0eb79f1b8605ec1987e095
mbozsoki <mbozsoki@myshopdisplay.com>
2020-01-05 12:58:20 +0100

    init frontend application with reactjs-ts-webpack-starter

---
+++ frontend/README.md
@@ -0,0 +1,19 @@
+# ReactJS with TypeScript and webpack starter
+
+ReactJS project starter with
+* TypeScript support
+* bundled with webpac
+* Prettier and ESLint configuration
+* watching is configured for webpack-dev-server
+
+# Start dev-server
+
+`npm run watch`
+
+# Build production
+
+`npm run build`
+
+# Build development
+
+`npm run devbuild`
---
+++ frontend/webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env = { mode: 'production' }, argv) => {
+    return {
+        mode: env.mode,
+
+        entry: {
+            app: './src/index.tsx',
+        },
+
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'main.js',
+            publicPath: '/',
+        },
+
+        devtool: env.mode === 'production' ? false : 'inline-source-map',
+
+        resolve: {
+            extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
+        },
+
+        module: {
+            rules: [
+                {
+                    test: /\.ts(x?)$/,
+                    exclude: /node_modules/,
+                    use: [
+                        {
+                            loader: 'ts-loader',
+                        },
+                    ],
+                },
+                {
+                    enforce: 'pre',
+                    test: /\.js$/,
+                    loader: 'source-map-loader',
+                },
+                {
+                    test: /\.(jpg|eot|woff|woff2|svg|ttf|png|webp)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: { name: '[name].[ext]' },
+                        },
+                    ],
+                },
+            ],
+        },
+
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: ' WB Simple loop displayer',
+                template: './src/index.html',
+            }),
+        ],
+
+        devServer: {
+            contentBase: path.join(__dirname, 'dist'),
+            compress: true,
+            port: 3000,
+        },
+    };
+};
